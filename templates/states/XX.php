30a286cce971845d3432e07e10489b6813e8da2c
Amira Abdin <ameraibrahem222@gmail.com>
2025-02-23 23:21:52 +0200

    QA.php

--- templates/states/XX.php
+++ templates/states/XX.php
@@ -14,20 +14,14 @@
 
 global $states;
 
-// Replace 'XX' code for your Country Code. E.g. 'NG' for Nigeria, 'CO' for Colombia, 'VE' for Venezuela, etc.
-// Please refer to WooCommerce country codes : https://github.com/woocommerce/woocommerce/blob/master/i18n/countries.php
-$states['XX'] = array(
-  // Some countries like Denmark or Belgium do not use the state field, 
-  // if your country doesn't use it either, leave the array empty.
-  // E.g. $states['DK'] = array();
+$states['QA'] = array(
+    'DA'  => __('Doha', 'states-cities-and-places-for-woocommerce'),
+    'RA'  => __('Al Rayyan', 'states-cities-and-places-for-woocommerce'),
+    'WA'  => __('Al Wakrah', 'states-cities-and-places-for-woocommerce'),
+    'KH'  => __('Al Khor', 'states-cities-and-places-for-woocommerce'),
+    'SH'  => __('Al Shamal', 'states-cities-and-places-for-woocommerce'),
+    'UM'  => __('Umm Salal', 'states-cities-and-places-for-woocommerce'),
+    'DAK' => __('Al Daayen', 'states-cities-and-places-for-woocommerce'),
+    'MS'  => __('Madinat ash Shamal', 'states-cities-and-places-for-woocommerce'),
+); 
 
-  // Otherwise, if the state field is used in your country:
-  // Add a State Code to each State Name using a logic sense. 
-  // E.g. 'FA' => 'Florida'
-  // You can use any standar like ISO, ANSI, etc. There is no an mandatory standar or system and you can use your own.
-  // Please see the U.S. state abbreviations to see an example:
-  // https://en.wikipedia.org/wiki/List_of_U.S._state_abbreviations
-  'AA' => 'State 1',
-  'BB' => 'State 2',
-  'CC' => 'State 3',
-);
